3daf0fa4aabfe455ff5557672af600bcc751dc64
chenrui <chenrui.qingqing@gmail.com>
2022-05-02 16:06:49 +0800

    Add bytecode UT

---
+++ src/test/java/org/lds56/mona/core/interpreter/ByteCodeTest.java
@@ -0,0 +1,225 @@
+package org.lds56.mona.core.interpreter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.lds56.mona.core.interpreter.ir.OpCode;
+import org.lds56.mona.core.runtime.types.MonaNull;
+import org.lds56.mona.core.runtime.types.MonaNumber;
+import org.lds56.mona.core.runtime.types.MonaObject;
+import org.lds56.mona.core.runtime.types.MonaString;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ByteCodeTest {
+
+    private static Map<String, Object> InputOf(Object... args) {
+        Map<String, Object> result = new HashMap<>();
+        for (int i=0; i<args.length/2; i++) {
+            result.put(args[2*i].toString(), args[2*i+1]);
+        }
+        return result;
+    }
+
+    @Test
+    public void testExpr() {
+
+        // b? 1+1 : 3
+        Instruction[] ins = new Instruction[] {
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.BRANCH_FALSE, 6),
+                Instruction.of(OpCode.LOAD_CONSTANT, 1),
+                Instruction.of(OpCode.LOAD_CONSTANT, 1),
+                Instruction.of(OpCode.BINARY_ADD),
+                Instruction.of(OpCode.RETURN_VALUE),
+                Instruction.of(OpCode.LOAD_CONSTANT, 3),
+                Instruction.of(OpCode.RETURN_VALUE)
+        };
+
+        MonaObject[] consts = new MonaObject[] {
+                MonaNull.NIL,
+                MonaNumber.newInteger(1),        // TODO: add common-used number
+                MonaNumber.newInteger(2),
+                MonaNumber.newInteger(3)
+        };
+        
+        String[] localNames = new String[] {"b"};
+        
+        ByteCode code = ByteCode.load(new BasicBlock[]{
+                BasicBlock.build(0, ins, consts, localNames).withName("__main__")
+        });
+
+        VirtualMach vm = VirtualMachFactory.createVM();
+        vm.load(code);
+        MonaObject res = vm.run(InputOf("b", true));
+
+        System.out.println(res.getValue());
+        Assertions.assertEquals(res.getValue(), 2);
+    }
+
+    @Test
+    public void testFuncCall() {
+
+        // fn f(x) {
+        //    x+1
+        //}
+        //f(y)
+        Instruction[] ins_main = new Instruction[] {
+                // Instruction.of(OpCode.LOAD_CONSTANT, 2),    // bb index
+                // Instruction.of(OpCode.LOAD_CONSTANT, 3),
+                Instruction.of(OpCode.MAKE_FUNCTION, 1),
+                Instruction.of(OpCode.STORE_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_LOCAL, 1),
+                Instruction.of(OpCode.CALL_FUNCTION, 1),
+                Instruction.of(OpCode.RETURN_VALUE)
+                // [Nil, 1, 'f', 2]
+        };
+        MonaObject[] consts_main = new MonaObject[] {
+                MonaNull.NIL,
+                MonaString.valueOf("f"),
+                MonaString.valueOf("y"),
+                // MonaNumber.newInteger(1),        // bb index
+        };
+
+        String[] localNames_main = new String[] {"f", "y"};
+
+        Instruction[] ins_f = new Instruction[] {
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_CONSTANT, 1),
+                Instruction.of(OpCode.BINARY_ADD),
+                Instruction.of(OpCode.RETURN_VALUE)
+        };
+
+        // [Nil, 1, 'f', 2]
+        MonaObject[] consts_f = new MonaObject[] {
+                MonaNull.NIL,
+                MonaNumber.newInteger(1)
+        };
+
+        String[] localNames_f = new String[] {"x"};
+
+        ByteCode code = ByteCode.load(new BasicBlock[]{
+                BasicBlock.build(0, ins_main, consts_main, localNames_main).withName("__f__"),
+                BasicBlock.build(ins_main.length, ins_f, consts_f, localNames_f).withName("__main__")
+        });
+
+        VirtualMach vm = VirtualMachFactory.createVM();
+        vm.load(code);
+        MonaObject res = vm.run(InputOf("y", 10));
+
+        System.out.println(res.getValue());
+        Assertions.assertEquals(res.getValue(), 11);
+    }
+
+
+    @Test
+    public void testWhile() {
+
+        // let x = 0;
+        //while x<y {
+        //    x += 1;
+        //}
+        //return x;
+        Instruction[] ins = new Instruction[] {
+                Instruction.of(OpCode.LOAD_CONSTANT, 1),
+                Instruction.of(OpCode.STORE_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_LOCAL, 1),
+                Instruction.of(OpCode.LESS_THAN),
+                Instruction.of(OpCode.BRANCH_FALSE, 11),
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.LOAD_CONSTANT, 2),
+                Instruction.of(OpCode.INPLACE_ADD),
+                Instruction.of(OpCode.STORE_LOCAL, 0),
+                Instruction.of(OpCode.JUMP_LOCAL, 2),
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.RETURN_VALUE),
+        };
+
+        MonaObject[] consts = new MonaObject[] {
+                MonaNull.NIL,
+                MonaNumber.newInteger(0),
+                MonaNumber.newInteger(1),
+        };
+
+        String[] localNames = new String[] {"x", "y"};
+
+        ByteCode code = ByteCode.load(new BasicBlock[]{
+                BasicBlock.build(0, ins, consts, localNames).withName("__main__")
+        });
+
+        VirtualMach vm = VirtualMachFactory.createVM();
+        vm.load(code);
+        MonaObject res = vm.run(InputOf("y", 10));
+
+        System.out.println(res.getValue());
+        Assertions.assertEquals(res.getValue(), 10);
+    }
+
+
+    @Test
+    public void testForIn() {
+
+//        let ans = 0;
+//        for x in l {
+//            if x % 2 == 1 {
+//                continue;
+//            }
+//            if x > 10 {
+//                break;
+//            }
+//            ans += x;
+//        }
+//        return ans;
+        Instruction[] ins = new Instruction[] {
+                Instruction.of(OpCode.LOAD_CONSTANT, 1),
+                Instruction.of(OpCode.STORE_LOCAL, 1),
+                Instruction.of(OpCode.LOAD_LOCAL, 0),
+                Instruction.of(OpCode.GET_ITERATOR),
+                Instruction.of(OpCode.NEXT_ITERATOR),
+                Instruction.of(OpCode.BRANCH_FALSE, 20),
+                Instruction.of(OpCode.STORE_LOCAL, 2),
+                Instruction.of(OpCode.LOAD_LOCAL, 2),
+                Instruction.of(OpCode.LOAD_CONSTANT, 2),
+                Instruction.of(OpCode.BINARY_MODULO),
+                Instruction.of(OpCode.LOAD_CONSTANT, 3),
+                Instruction.of(OpCode.BRANCH_EQUAL, 4),
+                Instruction.of(OpCode.LOAD_LOCAL, 2),
+                Instruction.of(OpCode.LOAD_CONSTANT, 4),
+                Instruction.of(OpCode.BRANCH_GREATER_THAN, 20),
+                Instruction.of(OpCode.LOAD_LOCAL, 1),
+                Instruction.of(OpCode.LOAD_LOCAL, 2),
+                Instruction.of(OpCode.INPLACE_ADD),
+                Instruction.of(OpCode.STORE_LOCAL, 1),
+                Instruction.of(OpCode.JUMP_LOCAL, 4),
+                Instruction.of(OpCode.LOAD_LOCAL, 1),
+                Instruction.of(OpCode.RETURN_VALUE),
+                };
+
+        MonaObject[] consts = new MonaObject[] {
+                MonaNull.NIL,
+                MonaNumber.newInteger(0),
+                MonaNumber.newInteger(2),
+                MonaNumber.newInteger(1),
+                MonaNumber.newInteger(10),
+                };
+
+        String[] localNames = new String[] {"l", "ans", "x"};
+
+        ByteCode code = ByteCode.load(new BasicBlock[]{
+                BasicBlock.build(0, ins, consts, localNames).withName("__main__")
+        });
+
+        VirtualMach vm = VirtualMachFactory.createVM();
+        vm.load(code);
+        List<Integer> l =  Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).collect(Collectors.toList());
+        MonaObject res = vm.run(InputOf("l", MonaObject.wrap(l)));
+
+        System.out.println(res.getValue());
+        Assertions.assertEquals(res.getValue(), 30);
+    }
+}
---
+++ src/test/java/org/lds56/mona/core/interpreter/CodeGenTest.java
@@ -0,0 +1,115 @@
+package org.lds56.mona.core.interpreter;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CodePointCharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.lds56.mona.core.codegen.InterpreterByteCodeGen;
+import org.lds56.mona.core.syntax.antlr.MonaLexer;
+import org.lds56.mona.core.syntax.antlr.MonaParser;
+import org.lds56.mona.core.syntax.ast.ASTParserVisitor;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CodeGenTest {
+
+    private static Map<String, Object> InputOf(Object... args) {
+        Map<String, Object> result = new HashMap<>();
+        for (int i=0; i<args.length/2; i++) {
+            result.put(args[2*i].toString(), args[2*i+1]);
+        }
+        return result;
+    }
+
+    Object interpret(String expr) {
+        return interpret(expr, new HashMap<>());
+    }
+
+
+    Object interpret(String expr, Map<String, Object> ctx) {
+
+        CodePointCharStream inputStream = CharStreams.fromString(expr);
+        MonaLexer lexer = new MonaLexer(inputStream);
+
+        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+        MonaParser parser = new MonaParser(tokenStream);
+
+        InterpreterByteCodeGen codegen = new InterpreterByteCodeGen();
+        ASTParserVisitor<ByteCodeBlock> vistor = new ASTParserVisitor<>(codegen);
+
+        ByteCodeBlock mainBlock = vistor.visit(parser.script());
+        ByteCode bc = codegen.generate(mainBlock);
+
+        // print
+        for (BasicBlock bb : bc.getBasicBlocks()) {
+            System.out.println("------------------");
+            System.out.println(bb.toString());
+        }
+        System.out.println("==================");
+
+        VirtualMach vm = VirtualMachFactory.createVM();
+        vm.load(bc);
+        return vm.run(ctx).getValue();
+
+        // System.out.println("Interpret expr: " + expr + ", result: " + result);
+        // return result;
+    }
+
+    @Test
+    public void testAddGen() {
+
+        String expr = "1+1";
+        Object result = interpret(expr);
+
+        Assertions.assertEquals(result, 2);
+    }
+
+    @Test
+    public void testWhile() {
+
+        String expr = "let a=1; while a<10 { a=a+1; } return a;";
+        // System.out.println(interpret(expr));
+        Object result = interpret(expr);
+
+        Assertions.assertEquals(result, 10);
+    }
+
+
+    @Test
+    public void testIf() {
+
+        String expr = "let a=1; if a % 2==1 { a=-1; } return a;";
+        // System.out.println(interpret(expr));
+        Object result = interpret(expr);
+
+        Assertions.assertEquals(result, -1);
+    }
+
+    @Test
+    public void testIfElse() {
+
+        String expr = "let a=0; if a % 2==1 { a=-1; } else { a=1; } return a;";
+        // System.out.println(interpret(expr));
+        Object result = interpret(expr);
+
+        Assertions.assertEquals(result, 1);
+    }
+
+    @Test
+    public void testFor() {
+
+        String expr = // "let a=[1,2,3,4,5]; " +
+                      "let ans=0; " +
+                      "for i in l { " +
+                      "     ans = ans + i; " +
+                      "} " +
+                      "return ans;";
+        // System.out.println(interpret(expr));
+        Object result = interpret(expr, InputOf("l", Arrays.asList(1,2,3,4,5)));
+
+        Assertions.assertEquals(result, 15);
+    }
+}
